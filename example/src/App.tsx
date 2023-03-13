99e319fe7e5b5a4e7620b9849e42a360bb515ef8
Eather <eathertoo@gmail.com>
2023-03-13 23:49:05 +0800

    feat: add example demo

---
+++ example/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ReactPhonenumberInput from 'react-phonenumber-input'
+
+function App() {
+  return (
+    <div className="App">
+      <ReactPhonenumberInput />
+    </div>
+  )
+}
+
+export default App
