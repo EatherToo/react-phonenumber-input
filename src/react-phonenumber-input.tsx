277537a0f0f7f81d445dbc97117c803684606dc2
Eather <eathertoo@gmail.com>
2023-03-13 23:48:52 +0800

    feat: add country select option

--- src/react-phonenumber-input.tsx
+++ src/react-phonenumber-input.tsx
@@ -1,5 +1,50 @@
-import React from 'react'
-
-export default function ReactPhonenumberInput() {
-  return <div>hello world</div>
+import React, { useState } from 'react'
+import './react-phonenumber-input.css'
+import getUnicodeFlagIcon from 'country-flag-icons/unicode'
+import { getCountryCallingCode, CountryCode } from 'libphonenumber-js'
+export type ReactPhonenumberInputProps = {
+  countries?: CountryCode[]
+  defaultCountry?: CountryCode
 }
+
+const ReactPhonenumberInput: React.FC<ReactPhonenumberInputProps> = (
+  props: ReactPhonenumberInputProps
+) => {
+  const [countryCode, setCountryCode] = useState(props.defaultCountry!)
+  return (
+    <div
+      style={{
+        width: '320px',
+        height: '30px',
+        margin: 'auto',
+      }}
+      className="react-phonenumber-input"
+    >
+      <div>
+        <select
+          value={countryCode}
+          onChange={(e) => {
+            setCountryCode(e.target.value as CountryCode)
+          }}
+          className="react-phonenumber-input_select"
+        >
+          {props.countries!.map((c) => {
+            return (
+              <option key={c} value={c}>
+                {getUnicodeFlagIcon(c)}
+              </option>
+            )
+          })}
+        </select>
+        <span>+{getCountryCallingCode(countryCode)}</span>
+      </div>
+    </div>
+  )
+}
+
+ReactPhonenumberInput.defaultProps = {
+  countries: ['CN', 'HK'],
+  defaultCountry: 'CN',
+}
+
+export default ReactPhonenumberInput
